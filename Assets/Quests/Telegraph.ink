3aa3b1cde4b72941f86382d2a0873026af342525
Brian Dean Jennings <brian@bdjnews.com>
2018-01-27 15:21:54 +1300

    Project init

---
+++ Assets/Quests/Telegraph.ink
@@ -0,0 +1,21 @@
+#left:Player
+#right:Colm
+#back:Desk
+
+-> Intro
+
+== Intro
+<H3> Telegraph </H3>
+
+(This is text)
++<i>Hello
+    You said 'Hello!'
+
++<i>Hey
+    You said hey!
+
+- Now we are back together.
+
+
+-> END
+
---
+++ Assets/Scripts/StageAssets.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Dialog {
+	public static class StageAssets {
+		private static Dictionary<string, GameObject> m_BackdropLookup;
+		private static Dictionary<string, GameObject> m_CharacterLookup;
+
+		public static Dictionary<string, GameObject> Backdrops {
+			get {
+				if (m_BackdropLookup == null) CheckInit();
+				return m_BackdropLookup;
+			}
+		}
+
+		public static Dictionary<string, GameObject> Characters {
+			get {
+				if (m_CharacterLookup == null) CheckInit();
+				return m_CharacterLookup;
+			}
+		}
+
+		private static void CheckInit() {
+			m_BackdropLookup = Resources.LoadAll<GameObject>("Backdrops").ToDictionary(go => go.name);
+			m_CharacterLookup = Resources.LoadAll<GameObject>("Characters").ToDictionary(go => go.name);
+		}
+	}
+}
